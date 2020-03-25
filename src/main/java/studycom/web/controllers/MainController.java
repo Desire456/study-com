8f10b9b5ade6a7601f04ea62dff4c9f7ba788a92
PavelPimKot <57503765+PavelPimKot@users.noreply.github.com>
2020-03-25 14:53:27 +0400

    ВЕДУТСЯ РАБОТЫ , ВСЕМ ЛЕЖАТЬ

--- src/main/java/studycom/web/controllers/MainController.java
+++ src/main/java/studycom/web/controllers/MainController.java
@@ -1,14 +1,29 @@
 package studycom.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import studycom.web.domain.User;
+import studycom.web.repos.UserRepository;
+
+import java.util.Map;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String home(Model model) {
-        return "home";
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping("/home")
+    public String home(Map<String, Object> model , @ModelAttribute("user") User user) {
+        if(user.getName()==null||user.getSurname() == null||user.getPassword()==null){
+            return "home";
+        }
+        userRepository.save(user);
+        return "lk";
     }
 
 }
--- src/main/java/studycom/web/domain/User.java
+++ src/main/java/studycom/web/domain/User.java
@@ -1,15 +1,57 @@
 package studycom.web.domain;
 
-/*import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;*/
+import javax.persistence.*;
+import java.io.Serializable;
 
-//@Entity
-public class User {
+@Entity
+@Table(name="users")
+public class User implements Serializable {
 
-   /* @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)*/
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
+    private String name;
+    private String surname;
+    private String password;//Лучше вынести в отдельны класс или ипользовать другую структуру
+
+    public User(String name , String surname , String password){
+        this.name = name ;
+        this.surname = surname;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User(){}
 }
