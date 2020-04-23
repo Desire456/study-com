e64c95438c446642dbaacb2c4a8b189cd184f41d
PavelPimKot <57503765+PavelPimKot@users.noreply.github.com>
2020-04-23 14:36:32 +0400

    LessonType to string FIX

--- src/main/java/studycom/web/domain/Lessons/LessonType.java
+++ src/main/java/studycom/web/domain/Lessons/LessonType.java
@@ -1,7 +1,17 @@
 package studycom.web.domain.Lessons;
 
 public enum LessonType {
-    PRACTICE,
-    LECTURE,
-    LAB
+    PRACTICE("PRACTICE"),
+    LECTURE("LECTURE"),
+    LAB("LAB");
+
+    private final String name;
+
+    LessonType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
